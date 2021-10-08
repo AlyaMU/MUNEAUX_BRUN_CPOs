29e7687388e030669b36c5dd2532698c1299e473
Alya <Alya@Alya-PC>
2021-10-08 12:53:02 +0200

    Depot TP1 Exo1

---
+++ TP1_manipNombresInt_MUNEAUX/src/tp1_manipnombresint_muneaux/TP1_manipNombresInt_MUNEAUX.java
@@ -0,0 +1,38 @@
+/*
+ * Alya MUNEAUX TDC
+ * TP1 : syntaxe de base
+ * Exo1 : saisie et manipulation de nombres
+ */
+package tp1_manipnombresint_muneaux;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alya
+ */
+public class TP1_manipNombresInt_MUNEAUX {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int entier1 = 0;
+        int entier2 = 0;
+        int quotient = 0;
+        int reste = 0;
+        Scanner valEntier1 = new Scanner(System.in);
+        System.out.println("\n Entrez une valeur entière :");
+        entier1 = valEntier1.nextInt(); // on demande a valEntier1 de donner le prochain entier
+        Scanner valEntier2 = new Scanner(System.in);
+        System.out.println("\n Entrez une valeur entière :");
+        entier2 = valEntier2.nextInt(); // on demande a valEntier2 de donner le prochain entier
+        System.out.println("La première valeur est : " + entier1);
+        System.out.println("La deuxième valeur est : " + entier2);
+        quotient = entier1/entier2;
+        reste = entier1%entier1;
+        System.out.println("Le quotient est : " + quotient);
+        System.out.println("Le reste est : " + reste);
+    }
+    
+}
