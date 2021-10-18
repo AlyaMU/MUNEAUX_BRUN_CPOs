73ad7dfeabef76819a45078004b24c96a452653f
Alya <Alya@Alya-PC>
2021-10-18 12:45:49 +0200

    Depot TP2

---
+++ TP2_manip_MUNEAUX_GOMEZ/src/tp2_manip_muneaux_gomez/Moussaka.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_muneaux_gomez;
+
+/**
+ *
+ * @author Alya
+ */
+public class Moussaka {
+    // Attributs :
+    int nbCalories;
+    
+     // Constructeur :
+    
+    public Moussaka(int valCalories) {
+        nbCalories = valCalories;
+    }
+}
