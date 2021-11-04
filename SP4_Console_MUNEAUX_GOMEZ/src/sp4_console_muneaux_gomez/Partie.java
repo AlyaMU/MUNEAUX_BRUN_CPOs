02b80e06d16af698b0b8ff6df54e65ad360e7f3f
Alya <Alya@Alya-PC>
2021-11-04 19:54:55 +0100

    Puissance 4 v1

--- SP4_Console_MUNEAUX_GOMEZ/src/sp4_console_muneaux_gomez/Partie.java
+++ SP4_Console_MUNEAUX_GOMEZ/src/sp4_console_muneaux_gomez/Partie.java
@@ -22,8 +22,7 @@ public class Partie {
     //Méthodes :
     
     void attribuerCouleursAuxJoueurs(){
-       String uneCouleur = joueurCourant.Couleur;
-       joueurCourant.affecterCouleur(uneCouleur);
+       joueurCourant.affecterCouleur(joueurCourant.Couleur);
     }
     
     void initialiserPartie(){
@@ -36,7 +35,8 @@ public class Partie {
         boolean resgrill = false;
         boolean partie = false;
         
-        while (partie!= true){ //la partie dur tant que personne n'a pas gagné ou que la grille n'est pas pleine
+        System.out.println("Bienvenue ! La partie va commencer.");
+        while (partie!= true){ // La partie dure tant que personne n'a gagné ou que la grille n'est pas pleine
             
         
             while (joueurCourant == ListeJoueurs[1]){ //tour du joueur 1
@@ -45,42 +45,40 @@ public class Partie {
                 while (rescol!=true){
                 
                     while (numColonne<=7 && numColonne>=1){
-                        System.out.println("Dans quel colonnes souhaitez-vous mettre votre Jeton? (Saisissez un nombres entre 1 et 7) ");
-                        numColonne = numeroColonne.nextInt(); //le joueur choisi la colonne dans laquel il place le jeton, on vérifie bien que la colonne existe
+                        System.out.println(" Dans quelles colonnes souhaitez-vous mettre votre Jeton ? (Saisissez un nombre entre 1 et 7) ");
+                        numColonne = numeroColonne.nextInt(); // Le joueur choisit la colonne dans laquelle il place le jeton, on vérifie bien que la colonne existe
                     }
             
-                    // rescol = grilleJeu.ajouterJetonDansColonne(jetonCourant, numColonne); //on vérifie que la colonne n'est pas pleine
+                    // rescol = grilleJeu.ajouterJetonDansColonne(jetonCourant, numColonne); // On vérifie que la colonne n'est pas pleine
                 }
-                resgrill = grilleJeu.etreRemplie(); //on vérifie si la grille est pleine
+                resgrill = grilleJeu.etreRemplie(); // On vérifie si la grille est pleine
                 if (resgrill == true){
-                   
                     break;
                 }
-                joueurCourant = ListeJoueurs[2]; //on finit son tour en passant la main au 2eme joueur
+                joueurCourant = ListeJoueurs[2]; // On finit son tour en passant la main au 2eme joueur
                 
             }
             
-              while (joueurCourant == ListeJoueurs[2]){
-                    Scanner numeroColonne = new Scanner(System.in);
-            
-                    while (rescol!=true){
-                
-                        while (numColonne<=7 && numColonne>=1){
-                            System.out.println("Dans quel colonnes souhaitez-vous mettre votre Jeton? (Saisissez un nombres entre 1 et 7) ");
-                            numColonne = numeroColonne.nextInt(); 
-                        }
-            
-                        // rescol = grilleJeu.ajouterJetonDansColonne(jetonCourant, numColonne); 
-                    }
-                    resgrill = grilleJeu.etreRemplie();
-                    if (resgrill == true){
-                
-                        break;
-                    }
+            while (joueurCourant == ListeJoueurs[2]){
+                  Scanner numeroColonne = new Scanner(System.in);
+
+                  while (rescol!=true){
+
+                      while (numColonne<=7 && numColonne>=1){
+                          System.out.println("Dans quelles colonnes souhaitez-vous mettre votre Jeton ? (Saisissez un nombre entre 1 et 7) ");
+                          numColonne = numeroColonne.nextInt(); 
+                      }
+
+                      // rescol = grilleJeu.ajouterJetonDansColonne(jetonCourant, numColonne); 
+                  }
+                  resgrill = grilleJeu.etreRemplie();
+                  if (resgrill == true){
+                      break;
+                  }
                 joueurCourant = ListeJoueurs[1];
-               }
+             }
         }
-        grilleJeu.viderGrille(); //on vide la grille à la fin de la partie
+        grilleJeu.viderGrille(); // On vide la grille à la fin de la partie
     
     }
 }
