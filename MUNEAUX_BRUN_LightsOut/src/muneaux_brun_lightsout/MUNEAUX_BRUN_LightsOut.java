7c3e058298083ae909c44f84410136b8949cbb41
Alya <Alya@Alya-PC>
2021-11-30 16:23:43 +0100

    Lights Out z'est partiiiiiii

---
+++ MUNEAUX_BRUN_LightsOut/src/muneaux_brun_lightsout/MUNEAUX_BRUN_LightsOut.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package muneaux_brun_lightsout;
+
+/**
+ *
+ * @author Alya
+ */
+public class MUNEAUX_BRUN_LightsOut {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
