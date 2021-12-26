3e65f9bcc081a4cc001425b2c31e5a7a37ec69a9
Alya <Alya@Alya-PC>
2021-12-26 16:02:45 +0100

    Ajout tri des meilleurs coups et meilleurs temps

--- MUNEAUX_BRUN_LightsOut/src/muneaux_brun_lightsout/FichierLu.java
+++ MUNEAUX_BRUN_LightsOut/src/muneaux_brun_lightsout/FichierLu.java
@@ -12,31 +12,36 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class FichierLu extends FichierEcrit {
+
+    ArrayList<String> coupsParties = new ArrayList<String>();
+    ArrayList<String> chronoParties = new ArrayList<String>();
+
     void lireFichier() {
         int compteur = 0;
-        ArrayList<String> coupsParties = new ArrayList<String>();
-        ArrayList<String> chronoParties = new ArrayList<String>();
-    try {
-      File myObj = new File("FichierLightsOut.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        if (compteur%2 == 0) { // Le chiffre lu correspond a un nombre de coups
-            coupsParties.add(data);
+        try {
+            File myObj = new File("FichierLightsOut.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (compteur % 2 == 0) { // Le chiffre lu correspond a un nombre de coups
+                    coupsParties.add(data);
+                } else {
+                    chronoParties.add(data);
+                }
+                compteur += 1;
+            }
+            Collections.sort(coupsParties);
+            Collections.sort(chronoParties);
+            System.out.println(coupsParties);
+            System.out.println(chronoParties);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
-        else {
-            chronoParties.add(data);
-        }
-        compteur += 1;
     }
-      System.out.println(coupsParties);
-      System.out.println(chronoParties);
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-  }
+
 }
